d9039bb432c13ebaa761cacbf55422fe4c0cf98e
Danil Ivanof <d_ivanof@wargaming.net>
2018-11-22 11:07:22 +0300

    Move swagger from aiohug to separate package 🚀

---
+++ README.rst
@@ -0,0 +1,15 @@
+aiohug
+======
+
+|pipeline status| |coverage report|
+
+.. |pipeline status| image:: https://gitlab.com/nonamenix/aiohug_swagger/badges/master/pipeline.svg
+   :target: https://gitlab.com/nonamenix/aiohug_swagger/commits/master
+.. |coverage report| image:: https://gitlab.com/nonamenix/aiohug_swagger/badges/master/coverage.svg
+   :target: https://gitlab.com/nonamenix/aiohug_swagger/commits/master
+
+Goal:
+======
+
+Generate swagger annotation for docs generation
+
---
+++ aiohug_swagger/handlers.py
@@ -0,0 +1,46 @@
+import os
+import yaml
+from aiohttp import web
+
+from aiohug import RouteTableDef
+from aiohug import swagger
+
+routes = RouteTableDef()
+
+
+@swagger.spec(exclude=True)
+@routes.get("/swagger.json")
+async def swagger_json(request):
+    return swagger.generate_swagger(request.app)
+
+
+@swagger.spec(exclude=True)
+@routes.get("/swagger.yaml")
+async def swagger_yaml(request):
+    return web.Response(
+        text=yaml.dump(swagger.generate_swagger(request.app)), content_type="text/yaml"
+    )
+
+
+async def _render_template(template):
+    template_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "templates", template
+    )
+    with open(template_path) as template:
+        return template.read().replace("{{ swagger_url }}", "/swagger.json")
+
+
+@swagger.spec(exclude=True)
+@routes.get("/")
+async def root():
+    return web.Response(
+        text=await _render_template("swaggerui.html"), content_type="text/html"
+    )
+
+
+@swagger.spec(exclude=True)
+@routes.get("/redoc.html")
+async def root():
+    return web.Response(
+        text=await _render_template("redoc.html"), content_type="text/html"
+    )
